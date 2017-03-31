61402304c53a96c77a47162209ae30108e1baa26
apeckham <apeckham@gmail.com>
2017-03-31 11:21:20 -0700

    remove old diff lib

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [tempfile "0.2.0"]
                  [hickory "0.7.0"]
-                 [fipp "0.6.8"]
-                 [gui-diff "0.6.7"]]
+                 [fipp "0.6.8"]]
   :main ^:skip-aot html-diff.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
