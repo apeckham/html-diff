8f62771ed2f32b2bf0e8d5cf5e1032bfe34ed2aa
apeckham <apeckham@gmail.com>
2017-03-31 11:21:01 -0700

    Launch opendiff and then exit

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [tempfile "0.2.0"]
                  [hickory "0.7.0"]
+                 [fipp "0.6.8"]
                  [gui-diff "0.6.7"]]
   :main ^:skip-aot html-diff.core
   :target-path "target/%s"
--- src/html_diff/core.clj
+++ src/html_diff/core.clj
@@ -1,16 +1,23 @@
 (ns html-diff.core
   (:gen-class)
   (:require [hickory.core :as hickory]
-            [gui.diff :refer [gui-diff-data]]))
+            [clojure.java.shell :refer [sh]]
+            [fipp.edn :as fipp]
+            [tempfile.core :refer [tempdir with-tempfile]]))
 
 (defn parse [url]
   (println (str "Fetching " url))
   (-> url slurp hickory/parse hickory/as-hiccup))
 
+(defn diff [text1 text2]
+  (with-tempfile [dir (tempdir)]
+    (spit (str dir "1") (with-out-str (fipp/pprint text1)))
+    (spit (str dir "2") (with-out-str (fipp/pprint text2)))
+    (future (sh "opendiff" (str dir "1") (str dir "2")))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [[url1 url2] args]
-    (gui-diff-data (parse url1) (parse url2))))
-
-
+    (diff (parse url1) (parse url2)))
+  (shutdown-agents))
