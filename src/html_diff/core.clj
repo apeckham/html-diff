1b44c1ea416698472313dd76bae67fd07d8e5dee
apeckham <apeckham@gmail.com>
2017-04-01 00:26:30 -0700

    Remove "\n" lines from diff

--- src/html_diff/core.clj
+++ src/html_diff/core.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [hickory.core :as hickory]
             [clojure.java.shell :refer [sh]]
+            [clojure.string :refer [replace]]
             [fipp.edn :as fipp]
             [tempfile.core :refer [tempdir with-tempfile]]))
 
@@ -9,11 +10,16 @@
   (println (str "Fetching " url))
   (-> url slurp hickory/parse hickory/as-hiccup))
 
+(defn spit-hiccup [path text]
+  (spit path (replace (with-out-str (fipp/pprint text)) #" +\"\\n\"\n" "")))
+
 (defn diff [text1 text2]
   (with-tempfile [dir (tempdir)]
-    (spit (str dir "1") (with-out-str (fipp/pprint text1)))
-    (spit (str dir "2") (with-out-str (fipp/pprint text2)))
-    (future (sh "opendiff" (str dir "1") (str dir "2")))))
+    (let [file1 (str dir "1")
+          file2 (str dir "2")]
+      (spit-hiccup file1 text1)
+      (spit-hiccup file2 text2)
+      (future (sh "opendiff" file1 file2)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
